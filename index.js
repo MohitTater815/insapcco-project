2c9e45cb8380448765992e1fcab0f8bb5296e852
MohitTater815 <mohittater815@gmail.com>
2025-02-08 15:47:19 +0530

    / api

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "main page"
+    });
+});
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
 });
